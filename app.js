0b13204cbadd1328a0d281238eb63088c2ed9720
puhtreeseeya <puhtreeseeya@gmail.com>
2017-08-01 09:47:15 -0400

    completed part 3

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require( 'express' );
 const app = express();
 const morgan = require('morgan');
-const volleyball = require('volleyball');
+//const volleyball = require('volleyball');
 
 app.listen(3000);
 
@@ -10,18 +10,21 @@ app.listen(3000);
 //   next();
 // })
 
-morgan(function(tokens, req, res) {
-  return [tokens.method(req, res),
-          tokens.url(req, res),
-          tokens.status(req, res)
-         ].join(' ');
-});
+// morgan(function(tokens, req, res) {
+//   return [tokens.method(req, res),
+//           tokens.url(req, res),
+//           tokens.status(req, res)
+//          ].join(' ');
+// });
+
+app.use(morgan('combined')); 
+ 
 
 app.use('/special/', function(req, res, next) {
   console.log("you reached the special area.");
   next();
-})
+}); 
 
 app.get('/', function(req, res) {
   res.send('server listening \n');
-})
+}); 
